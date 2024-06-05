26ed075abd10c5eee6fb3339d8930e3a712ca7a7
trane102 <maxs3234@gmail.com>
2024-06-05 16:21:17 +0500

    JS_ADD

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let score = 0;
+let score = localStorage.getItem("score1");
 
 document.getElementById('clickButton').addEventListener('click', function() {
   score++;
   document.getElementById('score').textContent = 'Кликов: ' + score;
+  localStorage.setItem("score1", score);
 });
